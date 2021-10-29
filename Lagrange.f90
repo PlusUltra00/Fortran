1dc58e93a171f5de178d68e5f575d9309244ba0e
PlusUltra00 <89250932+PlusUltra00@users.noreply.github.com>
2021-10-30 01:37:41 +0900

    master

--- Lagrange.f90
+++ Lagrange.f90
@@ -1,49 +1,49 @@
 program ex7_3
-	implicit none
-	
-	integer k
-	integer, parameter :: n = 10
-	double precision xp(1:n), yp(1:n)
-	integer, parameter :: out = 100
-	double precision x, y
-	
-	open(10, file = 'input7_3.txt', action = 'read')
-	do k = 1, n
-		read(10, *) xp(k), yp(k)
-	end do
-	close(10)
-	
-	open(20, file = 'output7_3.csv', status = 'replace')
-	do k = 0, out
-		x = xp(1) + k * (xp(n) - xp(1)) / dble(out)
-		y = lagrange(x)
-		write(20, *) x, ',', y
-	end do
-	close(20)
-	
+    implicit none
+    
+    integer k
+    integer, parameter :: n = 10
+    double precision xp(1:n), yp(1:n)
+    integer, parameter :: out = 100
+    double precision x, y
+    
+    open(10, file = 'input7_3.txt', action = 'read')
+    do k = 1, n
+        read(10, *) xp(k), yp(k)
+    end do
+    close(10)
+    
+    open(20, file = 'output7_3.csv', status = 'replace')
+    do k = 0, out
+        x = xp(1) + k * (xp(n) - xp(1)) / dble(out)
+        y = lagrange(x)
+        write(20, *) x, ',', y
+    end do
+    close(20)
+    
 contains
 
 function lagrange(xin)
-	implicit none
-	
-	integer in, jn
-	double precision, intent(in) :: xin
-	double precision lagrange
-	double precision L
-	
-	lagrange = 0.0d0
-	
-	do in = 1, n
-		L = 1.0d0
-	
-		do jn = 1, n
-			if(in /= jn) then
-				L = L * (xin - xp(jn)) / (xp(in) - xp(jn))
-			end if
-		end do
-	
-		lagrange = lagrange + L * yp(in)
-	end do
+    implicit none
+    
+    integer in, jn
+    double precision, intent(in) :: xin
+    double precision lagrange
+    double precision L
+    
+    lagrange = 0.0d0
+    
+    do in = 1, n
+        L = 1.0d0
+    
+        do jn = 1, n
+            if(in /= jn) then
+                L = L * (xin - xp(jn)) / (xp(in) - xp(jn))
+            end if
+        end do
+    
+        lagrange = lagrange + L * yp(in)
+    end do
 
 end function
 
--- Newton's_Method.f90
+++ Newton's_Method.f90
@@ -1,47 +1,47 @@
 program newton
-	implicit none
-	
-	integer i, n
-	double precision x, d
-	double precision :: eps = 1.0d-8
-	double precision f, df
-	
-	n = 1000
-	
-	write(*,*)"初期値x0を入力してください．"
-	read(*,*) x
-	
-	do i = 1, n
-		d = - f(x) / df(x)
-		x = x + d
-		write(*, *) x
-		if( abs(d) < eps ) exit
-	end do
-	
-	if( i < n ) then
-		write(*, '(A12,F12.8)') "求める解は", x
-	else
-		write(*, *) "解が収束しませんでした．"
-		write(*, *) "なおxの値は", x
-		
-	end if
+    implicit none
+    
+    integer i, n
+    double precision x, d
+    double precision :: eps = 1.0d-8
+    double precision f, df
+    
+    n = 1000
+    
+    write(*,*)"初期値x0を入力してください．"
+    read(*,*) x
+    
+    do i = 1, n
+        d = - f(x) / df(x)
+        x = x + d
+        write(*, *) x
+        if( abs(d) < eps ) exit
+    end do
+    
+    if( i < n ) then
+        write(*, '(A12,F12.8)') "求める解は", x
+    else
+        write(*, *) "解が収束しませんでした．"
+        write(*, *) "なおxの値は", x
+        
+    end if
    
 end program newton
 
 function f(x)
-	implicit none
-	double precision, intent(in) :: x
-	double precision f
-	
-	f = x ** 3 - 6 * x ** 2 + 7 * x + 2.0d0
+    implicit none
+    double precision, intent(in) :: x
+    double precision f
+    
+    f = x ** 3 - 6 * x ** 2 + 7 * x + 2.0d0
 
 end function f
 
 function df(x)
-	implicit none
-	double precision, intent(in) :: x
-	double precision df
-	
-	df = 3 * x ** 2 - 12 * x + 7.0d0
+    implicit none
+    double precision, intent(in) :: x
+    double precision df
+    
+    df = 3 * x ** 2 - 12 * x + 7.0d0
 
 end function df
