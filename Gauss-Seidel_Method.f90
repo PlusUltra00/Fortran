a5bf500bce3276b9572503833e4087f4814ec188
PlusUltra00 <89250932+PlusUltra00@users.noreply.github.com>
2021-10-30 01:36:27 +0900

    master

--- Gauss-Seidel_Method.f90
+++ Gauss-Seidel_Method.f90
@@ -1,65 +1,65 @@
 program GaussSeidel
-    implicit none
-    integer i, j, k, n
-    double precision a(100, 100)
-    double precision b(100), x(100)
-    double precision x_1(100)
-    double precision dr
-    double precision :: eps = 1.0d-8
-    integer :: kmax = 1000
-    
-    open(10, file = 'input.dat', action = 'read')
-    read(10, *) n
+	implicit none
+	integer i, j, k, n
+	double precision a(100, 100)
+	double precision b(100), x(100)
+	double precision x_1(100)
+	double precision dr
+	double precision :: eps = 1.0d-8
+	integer :: kmax = 1000
+	
+	open(10, file = 'input.dat', action = 'read')
+	read(10, *) n
 
-    do i = 1, n
-        read(10, *) (a(i, j), j = 1, n)
-        write(*, *) (a(i, j), j = 1, n)
-    end do
-    
-    do i = 1, n
-        read(10, *) b(i)
-        write(*, *) b(i)
-        x_1(i) = 1.0d0
-    end do
-    
-    close(10)
-    
-    do k = 1, kmax
-        do i = 1, n
-            x(i) = b(i)
-            do j = 1, i-1
-                x(i) = x(i) - a(i, j) * x(j)
-            end do
-            do j = i+1, n
-                x(i) = x(i) - a(i, j) * x_1(j)
-            end do
-            x(i) = x(i) / a(i, i)
-        end do
-        dr = 0.0d0
-        do i = 1, n
-            dr = dr + (x(i) - x_1(i)) ** 2
-            x_1(i) = x(i)
-        end do
-        dr = sqrt(dr)
-        if (dr < eps) exit
-    end do
-    
-    open(20, file = 'output.dat', action = 'write')
-    
-    if(k >= kmax) then
-        write(*, *) "解が収束しませんでした"
-        write(20, *) "解が収束しませんでした"
-    else
-        write(*, *) "Ax=bの解は以下の通りです"
-        write(20, *) "Ax=bの解は以下の通りです"
-        do i = 1, n
-            write(*, '(F12.4)') x(i)
-            write(20, '(F12.4)') x(i)
-        end do
-        write(*, *) "なお反復回数k=", k
-        write(20, *) "なお反復回数k=", k
-    end if
-    
-    close(20)
+	do i = 1, n
+		read(10, *) (a(i, j), j = 1, n)
+		write(*, *) (a(i, j), j = 1, n)
+	end do
+	
+	do i = 1, n
+		read(10, *) b(i)
+		write(*, *) b(i)
+		x_1(i) = 1.0d0
+	end do
+	
+	close(10)
+	
+	do k = 1, kmax
+		do i = 1, n
+			x(i) = b(i)
+			do j = 1, i-1
+				x(i) = x(i) - a(i, j) * x(j)
+			end do
+			do j = i+1, n
+				x(i) = x(i) - a(i, j) * x_1(j)
+			end do
+			x(i) = x(i) / a(i, i)
+		end do
+		dr = 0.0d0
+		do i = 1, n
+			dr = dr + (x(i) - x_1(i)) ** 2
+			x_1(i) = x(i)
+		end do
+		dr = sqrt(dr)
+		if (dr < eps) exit
+	end do
+	
+	open(20, file = 'output.dat', action = 'write')
+	
+	if(k >= kmax) then
+		write(*, *) "解が収束しませんでした"
+		write(20, *) "解が収束しませんでした"
+	else
+		write(*, *) "Ax=bの解は以下の通りです"
+		write(20, *) "Ax=bの解は以下の通りです"
+		do i = 1, n
+			write(*, '(F12.4)') x(i)
+			write(20, '(F12.4)') x(i)
+		end do
+		write(*, *) "なお反復回数k=", k
+		write(20, *) "なお反復回数k=", k
+	end if
+	
+	close(20)
 
 end program GaussSeidel
